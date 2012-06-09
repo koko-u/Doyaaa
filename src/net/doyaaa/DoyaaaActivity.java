0374022022dc2973984e5cc31f5d26cf182bd7ec
TAC <tac@tac42.net>
2012-06-09 11:32:34 +0900

    first android commit

---
+++ src/net/doyaaa/DoyaaaActivity.java
@@ -0,0 +1,13 @@
+package net.doyaaa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DoyaaaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
