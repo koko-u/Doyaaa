94ccbc4d4e62a2292fee8dbb6150b916c8ade5e5
cch-robo <robo@cch-lab.com>
2012-06-09 13:30:01 +0900

    カメラ起動までのコミット

--- src/net/doyaaa/DoyaaaActivity.java
+++ src/net/doyaaa/DoyaaaActivity.java
@@ -1,13 +1,51 @@
 package net.doyaaa;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class DoyaaaActivity extends Activity {
+	private final int CAMERA_RESULT = 1000;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        onExtraClick();
+    }
+    
+    
+    Uri mImageUri = null;
+    
+    public void onExtraClick() {
+        String filename = System.currentTimeMillis() + ".jpg";
+        
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, filename);
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        mImageUri = getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        
+        Intent intent = new Intent();
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+        startActivityForResult(intent, CAMERA_RESULT);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_RESULT) {
+            // ImageViewに表示するなら
+//            ImageView imageView = (ImageView) findViewById(R.id.main_image);
+//            imageView.setImageURI(mImageUri);
+        }
     }
 }
